f2e6865c1c02860f2facb117c0b9f1dce47db4b5
Alexandre Rieux <alexr.3165@gmail.com>
2018-08-21 08:13:02 +0100

    Add minimum swap

---
+++ python/Minimum Swap 2/solution.py
@@ -0,0 +1,10 @@
+inputCases = open('./input/input00.txt')
+inputArr = inputCases.read().split('\n')
+
+values = list(map(lambda x: int(x), inputArr[1].split()))
+
+
+def solve(arr):
+
+
+print(solve(values))
